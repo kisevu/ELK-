208f4254a8d09538edd6efbecbdf5b15c5ea8984
ameda-kevin <amedakevin@gmail.com>
2024-03-04 15:05:24 +0300

    updated and started the docker compose containing ELK containers and they successfully connect

---
+++ src/test/java/com/ameda/kisevu/ELKspringboot/ElkSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ameda.kisevu.ELKspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElkSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
